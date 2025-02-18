993935f4a398ce4aa06d2885b4c4a77d86345bfe
Alex Ainea <xo.ainea@gmail.com>
2025-02-18 22:30:43 +0300

    Jobs styling done - ehhhhh

--- job_app/src/components/JobListings.jsx
+++ job_app/src/components/JobListings.jsx
@@ -5,16 +5,18 @@ const JobListings = () => {
   return (
     <div>
         <h1/>Jobs:
-        {jobData.jobs.map((job) => (<div key={job.id} className='rounded-sm'>
-            <h2>{job.title}</h2>
-            <p>{job.type}</p>
-            <p>Description: {job.description}</p>
-            <p>Location: {job.location}</p>
-            <p>salary: {job.salary}</p>
-            <p>company: {job.company}</p>
-            <p>experience: {job.experience}</p>
-            <p>posted_date: {job.posted_date}</p>
-        </div>))}
+        <div className='flex flex-row justify-evenly flex-wrap'>
+            {jobData.jobs.map((job) => (<div key={job.id} className='rounded-xl border-2 h-56 w-96 my-2.5'>
+                <h2>{job.title}</h2>
+                <p>{job.type}</p>
+                <p>Description: {job.description}</p>
+                <p>Location: {job.location}</p>
+                <p>salary: {job.salary}</p>
+                <p>company: {job.company}</p>
+                <p>experience: {job.experience}</p>
+                <p>posted_date: {job.posted_date}</p>
+            </div>))}
+        </div>
     </div>
   )
 }
